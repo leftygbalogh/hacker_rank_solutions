e3619dafc2a74b2bac20b1d6fe889f53861d639a
lefty <geza.balogh@trayport.com>
2026-03-06 15:53:24 +0100

    Initial commit

---
+++ src/bin/proj_a/main.rs
@@ -0,0 +1,6 @@
+mod proj_template;
+
+fn main(){
+	screen::clear();
+	proj_template::printer(43);
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#[path = "bin/proj_a/proj_template.rs"]
+pub mod proj_template;
+
+#[path = "bin/nontrivial_palindrome/palindrome.rs"]
+pub mod palindrome;
