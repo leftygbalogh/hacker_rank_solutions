70cc610eb8394483e11c675aa1c50c1fe1e6eeaa
lefty <geza.balogh@trayport.com>
2026-03-14 15:16:16 +0100

    mutable vs non mutable variable passing

--- src/bin/index_search/index_search.rs
+++ src/bin/index_search/index_search.rs
@@ -1,36 +1,40 @@
 pub fn binarySearch(nums: &[i32], target: i32) -> i32 {
 	//default_solution(nums, &target)
+	let mut true_index = (nums.len()/2) as i32;
+	let (nums , target, true_index) = recursiveBinarySearch(nums, target, true_index);
+	println!("nums: {:?} target{} true index {}", nums, target, true_index);
 
-	let mut length = nums.len();
-	let mut index = length / 2;
-	if let Some(value) = is_value_at_index(nums, target, &mut index) {
-		return value;
-	}
-	else {
-		if  {  }
-	}
-	if nums[index] < target {
-		length = index / 2;
-		index = index + length;
-		if nums[index] == target {
-			return index as i32;
-		}
 
-	}
+
 
 	42
 
 
 }
 
-fn is_value_at_index(nums: &[i32], target: i32, index: &mut usize) -> Option<i32> {
-	if nums[index] == target {
-		return Some(true)
-	} else {
-		return Some(false)
-	}
-	None
-}
+fn recursiveBinarySearch(nums: &[i32], target: i32, true_index: i32) -> (&mut [i32], i32, i32) {
+	if nums.len() == 1 {
+		if nums[0] == target {
+			let x = (nums, target, true_index);
+			return x
+		}
+		else { return (nums, 0, -1) }
+		}
+
+	else {
+		let head: &mut[i32];
+		let tail: &mut[i32];
+		let mut length = nums.len()/2;
+		let true_index = true_index  + length as i32;
+		if nums[0] < target {
+			(head, tail) = nums.split_at_mut(length);
+			return recursiveBinarySearch(head, target, true_index);
+		}
+		else {
+			(head, tail) = nums.split_at_mut(length);
+			return recursiveBinarySearch(tail, target, true_index) }
+	} }
+
 
 fn default_solution(nums: &[i32], target: &i32) -> i32 {
 	let result = nums.binary_search(&target);
